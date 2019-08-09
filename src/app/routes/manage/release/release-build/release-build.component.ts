4f863586f7f4c21b75d08c02005b0e2a601344d0
yuanyujie <805631223@qq.com>
2019-08-09 17:25:35 +0800

    code

--- src/app/routes/manage/release/release-build/release-build.component.ts
+++ src/app/routes/manage/release/release-build/release-build.component.ts
@@ -16,7 +16,93 @@ export class ReleaseBuildComponent implements OnInit {
   public buildForm: FormGroup;
   public fileList = [];                       // 上传文件列表
   public uploadUrl = '/system/uploadfile?';   // 上传文件地址
-  public config = {};                         // 编辑器配置
+  public config = {
+    initialFrameHeight: 300,    // 初始化编辑器高度
+    autoHeightEnabled: false,   // 是否自动长高
+    imagePathFormat: '/system/uploadfile',
+    ActionName: 'uploadimage',
+    toolbars: [
+      [
+        'anchor', // 锚点
+        'undo', // 撤销
+        'redo', // 重做
+        'bold', // 加粗
+        'indent', // 首行缩进
+        'italic', // 斜体
+        'underline', // 下划线
+        'strikethrough', // 删除线
+        'subscript', // 下标
+        'fontborder', // 字符边框
+        'superscript', // 上标
+        'formatmatch', // 格式刷
+        'source', // 源代码
+        'blockquote', // 引用
+        'pasteplain', // 纯文本粘贴模式
+        'selectall', // 全选
+        'preview', // 预览
+        'horizontal', // 分隔线
+        'removeformat', // 清除格式
+        'time', // 时间
+        'date', // 日期
+        'insertrow', // 前插入行
+        'insertcol', // 前插入列
+        'mergeright', // 右合并单元格
+        'mergedown', // 下合并单元格
+        'deleterow', // 删除行
+        'deletecol', // 删除列
+        'splittorows', // 拆分成行
+        'splittocols', // 拆分成列
+        'splittocells', // 完全拆分单元格
+        'deletecaption', // 删除表格标题
+        'inserttitle', // 插入标题
+        'mergecells', // 合并多个单元格
+        'deletetable', // 删除表格
+        'cleardoc', // 清空文档
+        'insertparagraphbeforetable', // "表格前插入行"
+        'insertcode', // 代码语言
+        'fontfamily', // 字体
+        'fontsize', // 字号
+        'paragraph', // 段落格式
+        'simpleupload', // 单图上传
+        'insertimage', // 多图上传
+        'edittable', // 表格属性
+        'edittd', // 单元格属性
+        'link', // 超链接
+        'unlink', // 取消链接
+        'spechars', // 特殊字符
+        'searchreplace', // 查询替换
+        'map', // Baidu地图
+        'justifyleft', // 居左对齐
+        'justifyright', // 居右对齐
+        'justifycenter', // 居中对齐
+        'justifyjustify', // 两端对齐
+        'forecolor', // 字体颜色
+        'backcolor', // 背景色
+        'insertorderedlist', // 有序列表
+        'insertunorderedlist', // 无序列表
+        'fullscreen', // 全屏
+        'directionalityltr', // 从左向右输入
+        'directionalityrtl', // 从右向左输入
+        'rowspacingtop', // 段前距
+        'rowspacingbottom', // 段后距
+        'pagebreak', // 分页
+        'imagenone', // 默认
+        'imageleft', // 左浮动
+        'imageright', // 右浮动
+        'imagecenter', // 居中
+        'lineheight', // 行间距
+        'edittip ', // 编辑提示
+        'customstyle', // 自定义标题
+        'autotypeset', // 自动排版
+        'touppercase', // 字母大写
+        'tolowercase', // 字母小写
+        'background', // 背景
+        'scrawl', // 涂鸦
+        'inserttable', // 插入表格
+        'wordimage',
+      ]
+    ]
+  };                         // 编辑器配置
   private paramsId: string;                   // 当前页查询id
   public enclosureJsons = [];                 // 附件
   public imagesJsons = [];                    // 附件
@@ -60,7 +146,7 @@ export class ReleaseBuildComponent implements OnInit {
         const data = resp.result;
         data.rangeDate = [new Date(resp.result.beginDate), new Date(resp.result.endDate)];
         this.enclosureJsons = JSON.parse(resp.result.enclosureJson || '[]');
-        this.imagesJsons = JSON.parse(resp.result.imagesJsons || '[]');
+        this.imagesJsons = JSON.parse(resp.result.imagesJson || '[]');
         this.buildForm.patchValue({
           title: data.title,
           deputyTitle: data.deputyTitle,
@@ -159,7 +245,7 @@ export class ReleaseBuildComponent implements OnInit {
     params.beginDate = params.rangeDate[0].getTime();
     params.endDate = params.rangeDate[1].getTime();
     params.requestType = requestType;
-    if (this.paramsId) {
+    if (this.paramsId || this.buildForm.value.type === '2') {
       params.imagesJsons = this.imgFileList.map(item => item.response.result);
     }
     return params;
@@ -203,4 +289,9 @@ export class ReleaseBuildComponent implements OnInit {
   deleteFile(fileName: string) {
     this.enclosureJsons = this.enclosureJsons.filter(item => item.enclosureName !== fileName);
   }
+
+  ready(e) {
+    console.log(e);
+  }
 }
+
